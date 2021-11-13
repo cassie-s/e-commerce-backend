66929b0e93bbf61af886b15e8e65ce32039be764
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-11-13 11:27:45 -0600

    create belongsto items

--- models/index.js
+++ models/index.js
@@ -5,10 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Cateogry, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
+
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
 
